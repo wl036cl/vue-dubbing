a95b0e4abd693383753f98f1904378d5c63bdf4c
王力 <595296310@qq.com>
2018-05-31 18:27:31 +0800

    img4

--- src/views/layout/components/Footer.vue
+++ src/views/layout/components/Footer.vue
@@ -3,7 +3,7 @@
     <p>Copyright&nbsp;©&nbsp;2016 杭州秀秀科技有限公司. All Rights Reserved.</p>
     <p class="gov">
       <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602004537">
-        <img src="http://img5.peiyinxiu.com/web/images/201607201638a0f4ae29000b5be4.png"/>浙公网安备 33010602004537号
+        <img src="http://img4.peiyinxiu.com/web/images/201607201638a0f4ae29000b5be4.png"/>浙公网安备 33010602004537号
       </a>
       <a target="_blank" href="http://www.miibeian.gov.cn/">浙ICP备13027715号-2&nbsp;&nbsp;浙ICP证：浙B2-20170071</a>
     </p>
